0b4373bb9ddc5cbcf15e96a15a584583124f4e18
ageres7-dev <66694244+ageres7-dev@users.noreply.github.com>
2020-12-26 16:44:46 +0500

    Initial Commit

---
+++ AwordsCollectionApp/RammsteinLogo.swift
@@ -0,0 +1,75 @@
+//
+//  RammsteinLogo.swift
+//  AwordsCollectionApp
+//
+//  Created by Сергей on 26.12.2020.
+//  Copyright © 2020 Alexey Efimov. All rights reserved.
+//
+
+import SwiftUI
+
+struct RammsteinLogo: View {
+    let width: CGFloat
+    let height: CGFloat
+    var body: some View {
+        GeometryReader { geometry in
+            let size = min(geometry.size.height, geometry.size.width)
+            
+//            let cornerIndent = size * 0.26
+            let minIndent = size * 0.056
+            let longLine = size / 2 - minIndent / 2
+            let shortLine = longLine / 2
+            
+            Path { path in
+                
+                path.move(to: CGPoint(x: shortLine, y: shortLine - minIndent))
+                path.addLine(to: CGPoint(x: shortLine, y: 0))
+                path.addLine(to: CGPoint(x: longLine, y: 0))
+                
+                //
+                path.move(to: CGPoint(x: longLine + minIndent, y: 0))
+                path.addLine(to: CGPoint(x: size - shortLine, y: 0))
+                path.addLine(to: CGPoint(x: size - shortLine, y: shortLine))
+                path.addLine(to: CGPoint(x: size, y: shortLine))
+                path.addLine(to: CGPoint(x: size, y: longLine))
+                
+                //
+                path.move(to: CGPoint(x: size, y: size - longLine))
+                path.addLine(to: CGPoint(x: size, y: size - shortLine))
+                path.addLine(to: CGPoint(x: size - shortLine, y: size - shortLine))
+                path.addLine(to: CGPoint(x: size - shortLine, y: size))
+                path.addLine(to: CGPoint(x: size - longLine, y: size))
+                
+                //
+                path.move(to: CGPoint(x: longLine, y: size))
+                path.addLine(to: CGPoint(x: shortLine, y: size))
+                path.addLine(to: CGPoint(x: shortLine, y: size - (shortLine - minIndent) ))
+                
+            }
+            .stroke(
+                Color.black,
+                style: StrokeStyle(lineWidth: 10)
+            )
+            
+        }
+        .padding()
+        .frame(width: width, height: height)
+            
+    
+    }
+}
+
+
+//struct RammsteinStroke: ViewModifier {
+//    func body(content: Content) -> some View {
+//        content.stroke
+//
+//    }
+//}
+
+
+struct RammsteinLogo_Previews: PreviewProvider {
+    static var previews: some View {
+        RammsteinLogo(width: 300, height: 300)
+    }
+}
